e0e88cc4107a83ab23a9b66a0c6850a3737a4a55
msdtgames <60367536+msdtgames@users.noreply.github.com>
2020-02-07 15:32:25 -0500

    Update linknetwork.js

--- js/linknetwork.js
+++ js/linknetwork.js
@@ -1,56 +1,17 @@
-var langs = [
-  "en",                                         // English
-  "es",                                         // Spanish
-  "fr",                                         // French
-  "it",                                         // Italian
-  "de",                                         // German
-  "pl",                                         // Polish
-  "ru",                                         // Russian
-  "jp",                                         // Japanese
-  "zh",                                         // Chinese
-  "ko",                                         // Korean
-];
-var links = [
-  `https://msdtgames.github.io/${lang}/`,
-  `https://msdtgames.github.io/${lang}/news.html`,
-  `https://msdtgames.github.io/${lang}/games.html`,
-  `https://msdtgames.github.io/${lang}/blog.html`,
-  `https://msdtgames.github.io/${lang}/behindthescenes.html`,
-  `https://msdtgames.github.io/${lang}/artwork.html`,
-  `https://msdtgames.github.io/${lang}/about.html`,
-];
+var home = "https://msdtgames.github.io/en/";
+var news = "https://msdtgames.github.io/en/news.html";
+var games = "https://msdtgames.github.io/en/games.html";
+var blog = "https://msdtgames.github.io/en/blog.html";
+var behindthescenes = "https://msdtgames.github.io/en/behindthescenes.html";
+var artwork = "https://msdtgames.github.io/en/artwork.html";
+var about = "https://msdtgames.github.io/en/about.html";
 
-function getLang(language) { return langs[language]; }
-var lang = getLang(english);
-
-const english  = 0;
-const spanish  = 1;
-const french   = 2;
-const italian  = 3;
-const german   = 4;
-const polish   = 5;
-const russian  = 6;
-const japanese = 7;
-const chinese  = 8;
-const korean   = 9;
-
-const home            = 0;
-const news            = 1;
-const games           = 2;
-const blog            = 3;
-const behindthescenes = 4;
-const artwork         = 5;
-const about           = 6;
-
-function changeLang(language) { lang = getLang(language); }
-function page(html)
+function page(url)
 {
   var elem = document.createElement("a");
-  elem.setAttribute("href", links[html]);
-  
+  elem.setAttribute("href", url);
   document.body.appendChild(elem);
   elem.click();
-  
   document.body.deleteChild(elem);
   return;
 }
